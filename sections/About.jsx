40418afcefdd0d656c7e8c9c934d89c67973d166
Luis Herrera <luis378962@gmail.com>
2023-03-30 21:52:21 -0600

    Explore seccion final

--- sections/About.jsx
+++ sections/About.jsx
@@ -3,13 +3,13 @@
 import { motion } from "framer-motion";
 import { TypingText } from "../components";
 import styles from '../styles'
-import { fadeIn , staggetContainer } from '../utils/motion'
+import { fadeIn , staggerContainer } from '../utils/motion'
 
 const About = () => (
   <section className={`${styles.paddings} relative z-10`}>
     <div className="gradient-02 z-0"/>
     <motion.div
-      variants={staggetContainer}
+      variants={staggerContainer}
       initial="hidden"
       whileInView="show"
       viewport={{ once: false, amount: 0.25 }}
@@ -23,7 +23,7 @@ const About = () => (
         variants={fadeIn('up', 'tween', 0.2, 1)}
         className="mt-[8px] font-normal sm:text-[32px] text-[20px] text-center text-secondary-white"
       >
-        <span className="font-extrabold text-white">Metaverse</span>
+        <span className="font-extrabold text-white">Metaverse </span>
         is a new thing in the future, where you can enjoy the virtual world by feeling like it's really real, you can feel what you feel in this metaverse world, because this is really the <span className="font-extrabold text-white">madness of the metaverse</span> of today, using only <span className="font-extrabold text-white">VR</span> devices you can easily <span className="font-extrabold text-white">explore</span> the metaverse world you want, turn your dreams into reality. Let's explore the madness of the metaverse by scrolling down
       </motion.p>
 
@@ -36,7 +36,6 @@ const About = () => (
         
       </motion.img>
     </motion.div>
-    About section
   </section>
 );
 
