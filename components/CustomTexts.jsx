62d1b06368052876a88d1c9e84a304d9f13d0b6e
Luis Herrera <luis378962@gmail.com>
2023-03-31 23:59:06 -0600

    optimizacion realizada

--- components/CustomTexts.jsx
+++ components/CustomTexts.jsx
@@ -1,17 +1,16 @@
 'use client';
 
-import { motion } from "framer-motion";
-
-import { textContainer, textVariant2 } from "../utils/motion";
+import { motion } from 'framer-motion';
+import { textContainer, textVariant2 } from '../utils/motion';
 
 export const TypingText = ({ title, textStyles }) => (
   <motion.p
     variants={textContainer}
     className={`font-normal text-[14px] text-secondary-white ${textStyles}`}
-  > 
-    {Array.from(title).map((Letter, index) => (
+  >
+    {Array.from(title).map((letter, index) => (
       <motion.span variants={textVariant2} key={index}>
-        {Letter === '' ? '\u00A0' : Letter}
+        {letter === ' ' ? '\u00A0' : letter}
       </motion.span>
     ))}
   </motion.p>
